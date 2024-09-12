22817b0592dfcd2c55cd82e9df808bb46fcc6cec
bhoneminnaing <bhoneminnaing6@gmail.com>
2024-09-12 20:49:41 +0630

    2nd exercise is added

---
+++ shopping_cart_program.py
@@ -0,0 +1,5 @@
+item= input("Enter you want to buy:")
+price=float(input("Enter the price:"))
+quantity=int(input("Enter the quantity:"))
+total = price * quantity
+print(f"You have bought {quantity} for the {item} and Your total amount is {total}")
