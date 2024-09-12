53a54f7e9077750bb7c22308bf9929baebce9e06
bhoneminnaing <bhoneminnaing6@gmail.com>
2024-09-12 21:21:30 +0630

    Madlibs game and Quiz is added

---
+++ madlibs_game.py
@@ -0,0 +1,8 @@
+#Madlibs game is a word game where you create a story by failing in blanks with random words
+adj1 = input("Enter an adjective:")
+noun1 = input("Enter a noun:")
+adj2 = input("Enter an adjective:")
+verb1 = input("Enter a verb ending with 'ing':")
+adj3 = input("Enter a adjective:")
+
+print(f"Today I went to a {adj1} zoo. In an exhabit, I saw a {noun1}. {noun1} was {adj2} and {verb1}. I was {adj3}")
