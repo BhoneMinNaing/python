9457c6eccd01e8ae6472e9e83c09877f0af01ba5
bhoneminnaing <bhoneminnaing6@gmail.com>
2024-09-12 20:49:15 +0630

    First exercies is added

---
+++ rectangle_area_cal.py
@@ -0,0 +1,7 @@
+
+length = float(input("Enter the length:"))
+width=float(input("Enter the width:"))
+area= length * width
+print(area)
+
+#I am learning Python from Youtube. URL https://www.youtube.com/watch?v=ix9cRaBkVe0&list=PPSV
